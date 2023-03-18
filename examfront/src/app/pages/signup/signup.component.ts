f2d7d08be6073f5ae3b87a398c33087ade3791dc
Anagha More <moreanagha13@gmail.com>
2023-03-18 09:44:32 +0530

    using matsnackbar and sweetsalert for notification while registration

--- examfront/src/app/pages/signup/signup.component.ts
+++ examfront/src/app/pages/signup/signup.component.ts
@@ -1,5 +1,8 @@
+import { validateHorizontalPosition } from '@angular/cdk/overlay';
 import { Component, OnInit } from '@angular/core';
+import { MatSnackBar } from '@angular/material/snack-bar';
 import { UserService } from 'src/app/services/user.service';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-signup',
@@ -7,7 +10,7 @@ import { UserService } from 'src/app/services/user.service';
   styleUrls: ['./signup.component.css']
 })
 export class SignupComponent implements OnInit{
-  constructor(private userService:UserService){}
+  constructor(private userService:UserService, private snack:MatSnackBar){}
 
   public user={
     username: '',
@@ -23,22 +26,31 @@ export class SignupComponent implements OnInit{
   formSubmit(){
     console.log(this.user);
     if(this.user.username =='' || this.user.username==null){
-      alert('user is requires !!');
+      this.snack.open("UserName is required!!",'',{
+        duration:3000,
+        // verticalPosition:'top',
+        // horizontalPosition:'right',
+      });
       return;
     }
 
     //add user: user services
     this.userService.addUser(this.user).subscribe(
-      (data)=>{
+      (data:any)=>{
         //success
         console.log(data);
-        alert("success");
+        // alert("success");
+        Swal.fire('Successfully done!!','User id is '+data.id,'success');
+    
       },
 
       (error)=>{
         //error
         console.log(error);
-        alert('something went wrong');
+        // alert('something went wrong');
+        this.snack.open('Something went wrong!!','',{
+          duration:3000,
+        })
       }
     )
   }
